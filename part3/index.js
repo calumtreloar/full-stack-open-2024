036775ad73a47833eeaa4f0a8751d34eb1f1779d
calumtreloar <calumtreloar@gmail.com>
2024-02-23 11:45:30 +1030

    Added frontend to backend

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(express.static("dist"));
 
 app.use(express.json());
 morgan.token("personInfo", function (req, res) {
@@ -100,7 +104,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
